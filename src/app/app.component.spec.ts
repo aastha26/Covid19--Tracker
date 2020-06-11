e7c8ecf2a613bb15e4b4a28f19bc7760d381ed27
aastha26 <aasthajain906@gmail.com>
2020-06-11 22:34:45 +0530

    Covid-tracker

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,20 +16,20 @@ describe('AppComponent', () => {
 
   it('should create the app', () => {
     const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
+    const app = fixture.componentInstance;
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'covid-tracker'`, () => {
+  it(`should have as title 'covid19-tracker'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('covid-tracker');
+    const app = fixture.componentInstance;
+    expect(app.title).toEqual('covid19-tracker');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('covid-tracker app is running!');
+    const compiled = fixture.nativeElement;
+    expect(compiled.querySelector('.content span').textContent).toContain('covid19-tracker app is running!');
   });
 });
